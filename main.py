83d06f88207bfd921ea58aa3adf95b263e82f86b
kat09118 <katheenca09811@gmail.com>
2025-05-06 18:36:52 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ import random as r
 
 #variables 
 
-caracteres = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+caracteres = ("+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
 contraseña = ""
 
 cc=int(input("cuantos caracteres desearias q tenga tu contraseña"))
 
 # bucle for
 for i in range(cc):
-    cf =(r.choice(caracteres)))
+    character = r.choice(caracteres)
+    contraseña = contraseña+character
+print(contraseña)
+
